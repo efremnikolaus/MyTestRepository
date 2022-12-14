083c8bb37d707f5a026cc0acb91af7822ceb5189
Mykola Yefremov <efremovnikolaus@gmail.com>
2022-12-14 17:11:35 +0100

    fc

--- src/main/java/org.example/Main.java
+++ src/main/java/org.example/Main.java
@@ -1,8 +1,9 @@
-package org.example
+package org.example;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Good morning! :)");
+        System.out.println("Good morning!:)");
+        System.out.println("Of course.");
     }
 }
