38d2bc583ede7f9dfe523f99a4b60ddfb4b34020
Nikolai Efremov <efremovnikolaus@gmail.com>
2022-12-14 21:58:08 +0100

    edited

---
+++ Efremov_javaPro/readme.md
@@ -0,0 +1,5 @@
+# **My expectations from the Java Pro course**
+***
+
+- **To acquire new knowledge that will help me in my future job search.**
+- **Learn something new for yourself.**
---
+++ Efremov_javaPro/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        My expectations from the Java Pro course:
+
+
+        To acquire new knowledge that will help me in my future job search.
+                Learn something new for yourself.
+    }
+}
