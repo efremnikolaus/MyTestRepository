cabcc1ad582b2db59981184476c6bd5acbe5b029
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-01-01 16:51:32 +0100

    test commit

--- src/main/java/org.example/Main.java
+++ src/main/java/org.example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Good morning!:)");
-        System.out.println("Of course.");
+        for (String arg : args) {
+            System.out.println(arg + "is arg");
+        }
     }
 }
