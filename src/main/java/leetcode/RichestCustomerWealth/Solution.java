9dba096fef9dd7ff916e6f35473a6bad6dba9bb1
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-01-01 18:02:17 +0100

    richest customer code

---
+++ src/main/java/leetcode/RichestCustomerWealth/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.RichestCustomerWealth;
+
+/**/
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealthSoFar = 0;
+
+        for(int[] customer:accounts) {
+            int currentCustomerWealth = 0;
+
+            for(int bank:customer) {
+                currentCustomerWealth += bank;
+            }
+            maxWealthSoFar = Math.max(maxWealthSoFar, currentCustomerWealth);
+        }
+        return maxWealthSoFar;
+    }
+}
