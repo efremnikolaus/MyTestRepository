f13f9a0ec23a4299f45a674f4d141159029bc9e3
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-01-05 20:48:08 +0100

    rep

--- src/main/java/leetcode/ValidPalindrome/Main.java
+++ src/main/java/leetcode/ValidPalindrome/Main.java
@@ -3,18 +3,20 @@ package leetcode.ValidPalindrome;
 
 public class Main {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
-        int n=s.length();
-        int j=0;
-        int i=0;
+        s = s.toLowerCase();
+
+        int n = s.length();
+        int j = 0;
+        int i = 0;
         char c;
         char d;
-        while(i!=n-j-1 && i<n-j-1){
-            c=s.charAt(i);
-            if(c>='a'&& c<='z'||c>='0'&& c<='9'){
-                d=s.charAt(n-j-1);
-                if(d>='a'&& d<='z'||d>='0'&& d<='9'){
-                    if (!(c==d)){
+
+        while (i != n-j-1 && i < n-j-1){
+            c = s.charAt(i);
+            if (c >= 'a' && c <= 'z' || c >= '0' && c <= '9'){
+                d = s.charAt(n-j-1);
+                if (d >= 'a' && d <= 'z'||d >= '0' && d <= '9'){
+                    if (!(c == d)){
                         return false;
                     }
                 }
