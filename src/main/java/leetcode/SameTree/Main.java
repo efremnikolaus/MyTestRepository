53fb9431052004e15b7c37f7a1dbd8294d30f0c3
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-01-07 23:43:03 +0100

    new exercise

---
+++ src/main/java/leetcode/SameTree/Main.java
@@ -0,0 +1,28 @@
+package leetcode.SameTree;
+
+public class Main {
+    Boolean result = true;
+    private boolean val;
+    private Main left;
+    private Main right;
+
+    public boolean isSameTree (Main p, Main q) {
+        check(p,q);
+        return result;
+    }
+    private void check(Main p, Main q){
+        if (p == null && q == null) {
+            return;
+        }
+        if (p != null && q == null || q != null && p == null) {
+            result = false;
+            return;
+        }
+        if (p.val != q.val) {
+            result = false;
+            return;
+        }
+        check (p.left,q.left);
+        check (p.right,q.right);
+    }
+}
